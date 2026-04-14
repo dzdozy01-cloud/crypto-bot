d3676d1ad7567bff8d9671318471082e1def70b6
dzdozy01-cloud <dzdozy01@gmail.com>
2026-04-14 14:33:05 +0100

    Remove extra files

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-COINGECKO_API_URL = "https://api.coingecko.com/api/v3"
-CHECK_INTERVAL = 60
