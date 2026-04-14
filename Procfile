2492dca490aa65965000546859b788690a459754
dzdozy01-cloud <dzdozy01@gmail.com>
2026-04-14 14:50:05 +0100

    Force correct version in Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python bot.py
+worker: pip install python-telegram-bot==20.7 requests==2.31.0 && python bot.py
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(
+    install_requires=[
+        'python-telegram-bot==20.7',
+        'requests==2.31.0',
+    ]
+)
